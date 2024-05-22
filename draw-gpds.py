3a9bade50bb09c06e26b242a84ae498737d50c9d
Ayanami <lingbo_2022@sjtu.edu.cn>
2024-05-23 02:04:35 +0800

    finish the project

--- draw-gpds.py
+++ draw-gpds.py
@@ -18,8 +18,9 @@ if __name__=="__main__":
     test_mode = "nochange-bf"
     change_bf_data = [] # list of data
 
-    single_config = {"use_bf": True, "use_cache": True,
+    default_single_config = {"use_bf": True, "use_cache": True,
 "bf_size": 65536, "bf_func_num": 3}
+    single_config = deepcopy(default_single_config)
     config = []
     # 解析日志文件
     first_flag = True
@@ -65,16 +66,19 @@ if __name__=="__main__":
                 data = []
             test_mode = "nochange-bf"
             substr = line.strip().split(',')
-            single_config['use_bf'] = substr[0].split(":")[1] == 'true'
-            single_config['use_cache'] = substr[1].split(":")[1] == 'true'
+            true_re = r'true'
+            false_re = r'false'
+            single_config['use_bf'] = len(re.findall(true_re, substr[0])) > 0
+            single_config['use_cache'] = len(re.findall(true_re, substr[1])) > 0
+            print(single_config)
             continue
         if line.startswith('bf_size'):
             substr = line.strip().split(',')
             single_config['bf_size'] = int(substr[0].split(":")[1])
             single_config['bf_func_num'] = int(substr[1].split(":")[1])
             first_flag = False
-            if not first_flag:
-                config.append(deepcopy(single_config))
+            config.append(deepcopy(single_config))
+            single_config = deepcopy(default_single_config)
             continue
         else:
             operation = None
@@ -100,25 +104,43 @@ if __name__=="__main__":
     change_bf_data.append(deepcopy(data))
     # 绘制数据
     # 在相同配置下的数据
+    get_cache_bf = []
+    get_cache_nobf = []
+    get_nocache_nobf = []
+    # 生成三种config下get操作的对比图
+    def gen_title(config: dict):
+        if config["use_bf"] and config["use_cache"]:
+            return "use_bf, use_cache"
+        elif config["use_cache"]:
+            return "no_bf, use_cache"
+        else:
+            return "no_bf, no_cache"
     for single_config, data in zip(config, all_data):
+        if single_config["use_cache"] and single_config["use_bf"]:
+            get_cache_bf.append(deepcopy(data))
+        elif single_config["use_cache"]:
+            get_cache_nobf.append(deepcopy(data))
+        else:
+            get_nocache_nobf.append(deepcopy(data))
+    
+    for get1, get2, get3 in zip(get_cache_bf, get_cache_nobf, get_nocache_nobf):
         # print(single_config)
-        print(len(config))
+        # print(len(config))
         # print(data)
+        # len(get1) == 12
         for i in range(3):
             it = 4 * i
-            get_throughput = [d['Get'][0] for d in data[it: it+4]]
-            put_throughput = [d['Put'][0] for d in data[it: it+4]]
-            del_throughput = [d['Del'][0] for d in data[it: it+4]]
-            scan_throughput = [d['Scan'][0] for d in data[it: it+4]]
-            plt.plot(get_throughput, marker='o', label='Get')
-            plt.plot(put_throughput, marker='o', label='Put')
-            plt.plot(del_throughput, marker='o', label='Del')
-            plt.plot(scan_throughput, marker='o', label='Scan')
+            get1_throughput = [d['Get'][0] for d in get1[it: it+4]]
+            get2_throughput = [d['Get'][0] for d in get2[it: it+4]]
+            get3_throughput = [d['Get'][0] for d in get3[it: it+4]]
+            plt.plot(get1_throughput, marker='o', label="use_bf, use_cache")
+            plt.plot(get2_throughput, marker='o', label="no_bf, use_cache")
+            plt.plot(get3_throughput, marker='o', label="no_bf, no_cache")
+            title = f"GET Throughput with prebuilt_data_size={i*2000 + 1000}"
+            plt.title(title)
             plt.ylabel('Throughput (ops/s)')
             plt.xlabel("value size (bytes)")
             plt.xticks(range(4), ["10", "100", "1000", "10000"])
-            title = f'Thourghput with prebuilt_data_num={data[it]["prebuilt_data_num"]}'
-            plt.title(title)
             plt.legend()
             plt.savefig(os.path.join(savepath, title))
             plt.close()
